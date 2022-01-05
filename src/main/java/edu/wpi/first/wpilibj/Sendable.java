1b44fbacf39d974c75ed511d4c925f8f9df6e7b9
trc492 <trc492@live.com>
2022-01-05 15:38:25 -0800

    Patched up the missing/deprecated wpilib pieces to make it build.

---
+++ src/main/java/edu/wpi/first/wpilibj/Sendable.java
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2016-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj;
+
+public interface Sendable extends edu.wpi.first.util.sendable.Sendable {
+}

9853608e9e4cd25f75fe8640d5bf4fe21d422efa
trc492 <trc492@live.com>
2022-01-05 23:07:08 -0800

    Updated CommonLib. Minor cleanup.

--- src/main/java/edu/wpi/first/wpilibj/smartdashboard/SendableBuilder.java
+++ src/main/java/edu/wpi/first/wpilibj/smartdashboard/SendableBuilder.java
@@ -1,11 +1,5 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.smartdashboard;
 
-public interface SendableBuilder extends edu.wpi.first.util.sendable.SendableBuilder {
+public interface SendableBuilder extends edu.wpi.first.util.sendable.SendableBuilder
+{
 }

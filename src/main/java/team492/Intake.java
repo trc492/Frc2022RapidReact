ae6b6c5eca51383bd3c17123f225ba34b414efb9
trc492 <trc492@live.com>
2022-02-05 02:19:50 -0800

    Code reviewed Intake and cleaned up. Updated CommonLib. Updated FrcLib.

--- src/main/java/team492/Intake.java
+++ src/main/java/team492/Intake.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2022 Titan Robotics Club (http://www.titanrobotics.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package team492;
 
 import TrcFrcLib.frclib.FrcCANFalcon;
@@ -10,13 +32,17 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 - retract
 */
 
-class Intake{
-    
+public class Intake
+{
     public final String instanceName;
     public final FrcCANFalcon intakeMotor;
     public final FrcPneumatic intakePneumatic;
 
-    //constructor
+    /**
+     * Constructor: Create an instance of the object.
+     *
+     * @param instanceName specifies the instance name.
+     */
     public Intake(String instanceName)
     {
         this.instanceName = instanceName;
@@ -24,44 +50,47 @@ class Intake{
         intakePneumatic = new FrcPneumatic(
             instanceName + ".pneumatic", RobotParams.CANID_PCM, PneumaticsModuleType.CTREPCM,
             RobotParams.PNEUMATIC_INTAKE_RETRACT, RobotParams.PNEUMATIC_INTAKE_EXTEND);
-    }
+    }   //Intake
 
-    //public String toString
-    public String toString(){
+    @Override
+    public String toString()
+    {
         return instanceName;
-    }
+    }   //toString
     
-    //public void extend
-    public void extend(){
-        intakePneumatic.extend();
-    }
+    public void setPower(double power)
+    {
+        intakeMotor.set(power);
+    }   //setPower
 
-    //public void retract
-    public void retract(){
-        intakePneumatic.retract();
-    }
-
-    //public void setPower(double)
-    public void setPower(double pwr){
-        intakeMotor.set(pwr);
-    }
-
-    //public bool isExtended
-    public boolean isExtended(){
-        return intakePneumatic.isExtended();
-    }
-
-    //public void intakeIn
-    public void intakeIn(){
+    public void pickup()
+    {
         setPower(RobotParams.INTAKE_PICKUP_POWER);
-    }
+    }   //pickup
 
-    //public void intakeOut
-    public void intakeOut(){
+    public void spitOut()
+    {
         setPower(RobotParams.INTAKE_SPITOUT_POWER);
-    }
+    }   //spitOut
 
-    public void stop(){
+    public void stop()
+    {
         setPower(0);
-    }
-}
+    }   //stop
+
+    public void extend()
+    {
+        intakePneumatic.extend();
+    }   //extend
+
+    public void retract()
+    {
+        intakePneumatic.retract();
+    }   //retract
+
+    public boolean isExtended()
+    {
+        return intakePneumatic.isExtended();
+    }   //isExtended
+
+}   //class Intake
